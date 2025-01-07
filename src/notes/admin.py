96162d43b9ad313d234bbd124067550783dfd156
Kedr237 <ll.vanya237@gmail.com>
2025-01-07 18:19:52 +0300

    Change code style

--- src/notes/admin.py
+++ src/notes/admin.py
@@ -7,6 +7,7 @@ class NoteAdmin(admin.ModelAdmin):
     list_display = ("short_title", "owner", "parent", "created")
     search_fields = ("title", "owner__username")
     list_filter = ("owner", "parent", "created", "modified")
+    readonly_fields = ("created", "modified")
 
     def short_title(self, obj: Note):
         return obj.title[:20] + ("..." if len(obj.title) > 20 else "")
--- src/notes/models.py
+++ src/notes/models.py
@@ -5,8 +5,14 @@ User = get_user_model()
 
 
 class TimeMixin(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Creation date",
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Change date",
+    )
 
     class Meta:
         abstract = True
@@ -14,35 +20,35 @@ class TimeMixin(models.Model):
 
 class Note(TimeMixin):
     owner = models.ForeignKey(
-        verbose_name="Note owner",
         to=User,
         related_name="notes",
         on_delete=models.CASCADE,
-
+        verbose_name="Note owner",
     )
     title = models.CharField(
-        verbose_name="Note title",
         max_length=255,
+        verbose_name="Note title",
     )
     content = models.TextField(
-        verbose_name="Note content",
         blank=True,
         null=True,
+        verbose_name="Note content",
     )
     image = models.ImageField(
-        verbose_name="Image for header",
         upload_to="notes/images/",
         blank=True,
         null=True,
+        verbose_name="Header image",
     )
     parent = models.ForeignKey(
-        verbose_name="Parent note",
         to="self",
         null=True,
         blank=True,
         related_name="children",
         on_delete=models.CASCADE,
+        verbose_name="Parent note",
     )
 
     def __str__(self):
-        return f"{self.owner.username} | {self.title}"
+        return f"{self.owner.username} | \
+            {self.title[:20] + ('...' if len(self.title) > 20 else '')}"
