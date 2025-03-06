fec4a4a73b0b0cf0a83a8d89a1698cbc85f354d5
Kedr237 <ll.vanya237@gmail.com>
2025-03-06 14:58:07 +0300

    Quick fix

--- src/main.py
+++ src/main.py
@@ -23,4 +23,4 @@ app = FastAPI(
 app.include_router(get_all_routers())
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port='8000')
+    uvicorn.run(app, host='0.0.0.0', port=8000)
